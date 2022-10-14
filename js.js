cd78932f64b0f57548a6a6f2e6a587a861af8b8f
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-10-14 15:19:14 +0100

    Update js.js

--- js.js
+++ js.js
@@ -32,6 +32,23 @@ ReadFromAssets: function(f){
 
 }
 
+function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+}
+
+
+
 function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
