9527e70fd559bc4f75c5a69cf569af9a8f16b13c
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-10-17 14:10:55 +0100

    Update js.js

--- js.js
+++ js.js
@@ -95,7 +95,50 @@ flashArray = []
 }
 
 
+function onChange(event) {
+  var file = event.target.files[0];
+  var reader = new FileReader();
+  reader.onload = function(e) {
 
+
+    // The file's text will be printed here
+   fileSystem = JSON.parse(e.target.result);
+  };
+
+  reader.readAsText(file); 
+
+
+for ( var property in fileSystem ) {
+  tk.writeFile('filelist.txt',property+"\n",true); // Outputs: foo, fiz or fiz, foo
+}
+
+var r = tk.readFile("filelist.txt");
+tk.flash ("this far");
+r = r.split("\n");
+//tk.flash(newText);
+
+for (var i = ii; i < r.length; i++) {
+if(r[i].includes(".")){
+var btn = document.createElement("button");
+var t = document.createTextNode(r[i]);
+document.getElementById('slide-1').appendChild(btn);
+
+var filename = r[i].replace(/^.*[\\\/]/, '')
+btn.textContent = filename;
+btn.setAttribute("id", r[i]);
+btn.setAttribute("class", r[i]);
+
+//var textarea = document.getElementById("ta");
+
+var toLoad = r[i];
+btn.setAttribute("onclick", "tk.writeFile('Current.txt','"+toLoad+"',false); var readC = tk.readFile('Current.txt'); var current = readC.split('\n'); tk.flash(current[0]); detectMode(); editor.setValue('"+toLoad+"'); editor.setValue(tk.readFile('"+toLoad+"'));");
+//btn.setAttribute("onclick","alert('gets here');");
+ii = i+1;
+}
+}
+
+
+}
 
 tk.flash('Welcome to Maker+ Js')
 
