1c9e22a089eab1729b808bbdd51f19cd9660db2c
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-10-17 11:29:15 +0100

    Update js.js

--- js.js
+++ js.js
@@ -61,7 +61,7 @@ function onChange(event) {
 for ( var property in fileSystem ) {
   tk.writeFile('filelist.txt',property+"\n",true); // Outputs: foo, fiz or fiz, foo
 }
-
+/*
 var r = tk.readFile("filelist.txt");
 //tk.flash ("this far");
 r = r.split("\n");
@@ -84,7 +84,7 @@ btn.setAttribute("onclick", "tk.writeFile('Current.txt','"+toLoad+"',false); var
 //btn.setAttribute("onclick","alert('gets here');");
 ii = i+1;
 }
-
+*/
 }
 
 
