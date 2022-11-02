dc40c09c3284e939d32aaaa8864a8aa8cca32302
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-11-02 13:34:35 +0000

    Update js.js

--- js.js
+++ js.js
@@ -588,7 +588,7 @@ var row = editor.session.getLength() - 1
 var column = editor.session.getLine(row).length // or simply Infinity
 //editor.gotoLine(row + 1, column)
 editor.execCommand("gotolineend")
-if(readFile(sims+".txt")!="undefined"){
+if(tk.readFile(sims+".txt")!="undefined"){
 editor.session.insert(editor.getCursorPosition(), "\n"+tk.readFile(sims+".txt"))
 }
 }catch(err){
