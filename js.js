c13a3ca3e775c8e082b0764873198c74d3383186
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-11-08 10:15:49 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1523,6 +1523,7 @@ if (typeof(element) != 'undefined' && element != null)
 {
 element.textContent = splRead[1];
 editor.insert(splRead[0])
+editor.blur()
 editor.focus()
 tk.flash(str[0])
 codifying = 2
@@ -1540,10 +1541,11 @@ btn.setAttribute("style", "height:20px;");
 //var textarea = document.getElementById("ta");
 
 editor.insert(splRead[0])
+editor.blur()
 editor.focus()
 var toLoad = "as0";
 str2 = str
-btn.setAttribute("onclick", "editor.insert(document.getElementById(this.id).textContent); editor.focus() document.getElementById(this.id).textContent = splRead[codifying]; codifying = codifying+1; tk.flash(str2["+codifying+"])");
+btn.setAttribute("onclick", "editor.insert(document.getElementById(this.id).textContent); editor.blur(); editor.focus(); document.getElementById(this.id).textContent = splRead[codifying]; codifying = codifying+1; tk.flash(str2["+codifying+"])");
 //btn.setAttribute("onclick","alert('gets here');");
 //ii = i+1;
 tk.flash(str[0])
