80b1eca3c0a1a45a378486e7dd210a2fe4c5b570
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-11-15 04:51:26 +0000

    Update js.js

--- js.js
+++ js.js
@@ -73,9 +73,9 @@ async function toFlash(f) {
 
 for(var i = 0; i < flashArray.length; i++){
 if(flashArray.length < 2 ){
-await delay(500);
+await delay(1000);
 buttonElm.textContent = flashArray[flashArray.length]
-await delay(500);
+await delay(1000);
 buttonElm.textContent = flashArray[i]
 }else{
 
