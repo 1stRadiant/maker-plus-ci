774624c74fc44683e91a36b9e16e637c92812b4d
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-12-03 14:13:46 +0000

    Update js.js

--- js.js
+++ js.js
@@ -524,6 +524,21 @@ var toLoad = "as"+i;
 			tk.flash(err)
 		}
 	}
+
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/bigscience/bloom",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.json();
+	return result;
+}
+
+
 	
 	function func(){
 		
