42d35d91bde51824f8824b7cc35f9bc8cddcfc64
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-12-13 01:48:34 +0000

    Update js.js

--- js.js
+++ js.js
@@ -484,9 +484,9 @@ query({"inputs": inp}).then((response) => {
 
 tk.flash(response[0].generated_text)
 
-var spl = response[0].generated_text.split(inp)
+//var spl = response[0].generated_text.split(inp)
 
-editor.session.insert(editor.getCursorPosition(), spl[1])
+//editor.session.insert(editor.getCursorPosition(), spl[1])
 				editor.focus()
 				editor.blur()
 				editor.focus()
