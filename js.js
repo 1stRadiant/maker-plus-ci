1c8b7f5f7b89afc258faa394cf6b160db47ab86b
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-12-20 02:56:03 +0000

    Update js.js

--- js.js
+++ js.js
@@ -488,12 +488,19 @@ query({"inputs": inp, "parameters":{"do_sample": true}}).then((response) => {
 tk.flash(response[0].generated_text)
 
 var spl = response[0].generated_text.split(inp);
-if(spl[1].includes("\n\n")){
-spl = spl[1].split("\n\n")
+if(spl[1].includes(".\n")){
+spl = spl[1].split(".\n")
 editor.session.insert(editor.getCursorPosition(), spl[0])
 				editor.focus()
 				editor.blur()
 				editor.focus()
+}else{
+	editor.session.insert(editor.getCursorPosition(), spl[1])
+				editor.focus()
+				editor.blur()
+				editor.focus()
+	
+	
 }
 });
 }
