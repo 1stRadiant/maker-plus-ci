25a0f8df2242a2ea3c30089597880f26aac522c0
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-12-22 20:07:30 +0000

    Update js.js

--- js.js
+++ js.js
@@ -495,14 +495,14 @@ editor.session.insert(editor.getCursorPosition(), spl[0])
 				editor.blur()
 				editor.focus()
 				tk.flash(spl[0])
-				tk.writeFile("test.txt","User: "+spl[0],true)
+				tk.writeFile("test.txt",spl[0],true)
 }else{
 	editor.session.insert(editor.getCursorPosition(), spl[1])
 				editor.focus()
 				editor.blur()
 				editor.focus()
 				tk.flash(spl[1])
-								tk.writeFile("test.txt","User: "+spl[1],true)
+								tk.writeFile("test.txt",spl[1],true)
 	
 	
 }
