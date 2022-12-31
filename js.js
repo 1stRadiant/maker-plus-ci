421bb8ac157b14a7cffc0630598022fcfba7332d
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2022-12-31 11:02:28 +0000

    Update js.js

--- js.js
+++ js.js
@@ -509,14 +509,15 @@ editor.session.insert(editor.getCursorPosition(), spl[0])
 				tk.flash(spl[1])*/
 								//tk.writeFile(tk.readFile("linkedfile.txt"),spl[1],true)
 		toRtn = spl[1]
-			    tk.writeFile("toMessage.txt",toRtn,true)
-							  var outputText = tk.readFile("toMessage.txt");
-    document.getElementById("app").innerHTML = outputText;
-
+			    
 	
 }
 });
 	
+await delay(500);
+tk.writeFile("toMessage.txt",toRtn,true)
+							  var outputText = tk.readFile("toMessage.txt");
+    document.getElementById("app").innerHTML = outputText;
 
 
 	 
