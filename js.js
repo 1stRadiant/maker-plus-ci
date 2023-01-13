732a2fe77225ad0172a130cf3315089d347ab306
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-01-13 02:45:51 +0000

    Update js.js

--- js.js
+++ js.js
@@ -594,7 +594,7 @@ write = write + text[i]
   if(i >= text.length){
   clearInterval(timer);
 		//tk.writeFile("toMessage.txt",runMore+ " "+text,false)
-if(text.at(-1)!="." ||  text.at(-1)!="?" ||text.at(-1)!=":"){
+/*if(text.at(-1)!="." ||  text.at(-1)!="?" ||text.at(-1)!=":"){
 			var runMore = tk.readFile("toMessage.txt")
 			if(r2 < 3){
 						tk.writeFile("toMessage.txt",runMore+ " "+text,false)
@@ -606,7 +606,7 @@ if(text.at(-1)!="." ||  text.at(-1)!="?" ||text.at(-1)!=":"){
 																		r2 = 0;
 												}
 			}
-				
+				*/
 						
 				}
 				
