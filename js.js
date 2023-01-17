eb2299f85fcbd0cd7552f159f79dc0a2e7e6ba0b
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-01-17 07:25:08 +0000

    Update js.js

--- js.js
+++ js.js
@@ -599,6 +599,8 @@ write = write + text[i]
 		var toM = document.getElementById("ap").textContent.split("Multiply")
 				var toML = toM[toM.length-1]
 			 var toMLO = toML
+				var toML = toML.split(" =")
+				eval("alert(toML[0])")
 				var maT = toML.match(/^(?:(?!0\*0)\d+\*\d+,)*(?:(?!0\*0)\d+\* \d+)$|(?:(?!0\*0)\d+ \*\d+)$|(?:(?!0\*0)\d+ \* \d+)$|(?:(?!0\*0)\d+\*\d+)$/);
 alert(maT)
 if(maT){
