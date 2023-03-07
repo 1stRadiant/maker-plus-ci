32854898b32df84a824f6842c45fc5bf6617dc9e
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-03-07 16:00:16 +0000

    Update js.js

--- js.js
+++ js.js
@@ -530,8 +530,8 @@ async function query(data) {
 var r2 = 0;
 var r1 = 0;
 function runBloom(inp){
-	
-query({"inputs": inp, "parameters":{"do_sample": do_sample}}).then((response) => { 
+	//, "parameters":{"do_sample": do_sample}
+query({"inputs": inp}).then((response) => { 
 
 //tk.flash(response[0].generated_text)
 
