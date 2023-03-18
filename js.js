378a73761e7f098c88484e4880bc8d170e0c858a
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-03-18 17:25:19 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1550,9 +1550,9 @@ str = str.split("\n");
 var match = stringSimilarity.findBestMatch(prompt[1],str);
 match = match.bestMatch.target;
 tk.writeFile("Current.txt",tk.readFile(match),false);
-alert(tk.readFile("Current.txt"));
+//alert(tk.readFile("Current.txt"));
 interface();
-window.location.href = "index.html#restart";
+//window.location.href = "index.html#restart";
 
 };
 	
