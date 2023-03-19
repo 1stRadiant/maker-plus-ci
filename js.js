76327a35d0790e318d80cfe62f6b66068d8ebe15
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-03-19 01:40:47 +0000

    Update js.js

--- js.js
+++ js.js
@@ -349,9 +349,9 @@ if(window.location.toString().includes("prompt:")){
 var prompt = window.location.toString().split("prompt:")
 var str = tk.readFile("filelist.txt");
 str = str.split("\n");	
-/*var match = stringSimilarity.findBestMatch(prompt[1],str);
+var match = stringSimilarity.findBestMatch(prompt[1],str);
 match = match.bestMatch.target;
-tk.writeFile("Current.txt",tk.readFile(match),false);*/
+tk.writeFile("Current.txt",tk.readFile(match),false);
 //alert(tk.readFile("Current.txt"));
 //editor.setValue(tk.readFile("Current.txt"))
 //interface();
