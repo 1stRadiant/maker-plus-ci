c0b0e514ed667f6268281e94b7f0bc92c676e363
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-03-20 17:40:04 +0000

    Update js.js

--- js.js
+++ js.js
@@ -370,8 +370,15 @@ if(window.location.toString().includes("api-prompt:")){
 var prompt = window.location.toString().split("prompt:")
 fetch ("https://makerci.com/prompts-api")
 .then(x => x.text())
-.then(y => editor.setValue(y));
-
+.then(y => tk.writeFile("pText.txt",y,false););
+var match = stringSimilarity.findBestMatch(prompt[1],str);
+match = match.bestMatch.target;
+//alert(tk.readFile("Current.txt"));
+async function asSetV(){
+await delay(500);
+editor.setValue(tk.readFile("pText.txt"))
+interface();
+//detectMode();
 }
 
 try{
