ce9572c77cfa284e5090e356248223ae7a474eb3
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-03-20 20:06:33 +0000

    Update js.js

--- js.js
+++ js.js
@@ -365,28 +365,16 @@ await delay(1500);
 window.close();
 }
 }
-/*
+
 if(window.location.toString().includes("api-prompt:")){
 var prompt = window.location.toString().split("prompt:")
 fetch ("https://makerci.com/prompts-api")
 .then(x => x.text())
-.then(y => tk.writeFile("pText.txt",y,false));
-var match = stringSimilarity.findBestMatch(prompt[1],str);
-match = match.bestMatch.target;
-//alert(tk.readFile("Current.txt"));
-async function asSetV2(){
-await delay(500);
-editor.setValue(tk.readFile("pText.txt"))
-interface();
-//detectMode();
+.then(y => f = y.JSON.parse;
+editor.setValue(f.prompt["Linux Terminal"]);
+);
+
 }
-
-try{
-asSetV2();
-
-}catch(err){
-alert(err)
-}*/
 
 try{
 asSetV();
