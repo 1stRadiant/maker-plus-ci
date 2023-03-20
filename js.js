c718cb6201ed8017d2b6db08d37132bd2c5248f7
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-03-20 20:34:21 +0000

    Update js.js

--- js.js
+++ js.js
@@ -375,7 +375,7 @@ var match = stringSimilarity.findBestMatch(prompt[1],str);
 match = match.bestMatch.target;
 //alert(tk.readFile("Current.txt"));
 async function asSetV2(){
-await delay(500);
+await delay(1000);
 editor.setValue(tk.readFile("pText.txt"))
 interface();
 //detectMode();
