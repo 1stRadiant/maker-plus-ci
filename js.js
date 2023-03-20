9ec3956af2a7259280d0ec9529fcc61fb5a55a94
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-03-20 17:20:23 +0000

    Update js.js

--- js.js
+++ js.js
@@ -368,12 +368,12 @@ window.close();
 
 if(window.location.toString().includes("api-prompt:")){
 var prompt = window.location.toString().split("prompt:")
-fetch ("https://makerci.com/prompts-api")
+/*fetch ("https://makerci.com/prompts-api")
 .then(x => x.text())
 .then(y => f = y.JSON.parse;
 editor.setValue(f.prompt["Linux Terminal"]);
 );
-
+*/
 }
 
 try{
