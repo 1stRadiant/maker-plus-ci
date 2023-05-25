d5ce2bc80ad444a5b0bccb440bd696670c027826
1stRadiant <66203743+1stRadiant@users.noreply.github.com>
2023-05-25 18:28:04 +0100

    Update js.js

--- js.js
+++ js.js
@@ -647,7 +647,7 @@ var r2 = 0;
 var r1 = 0;
 function runBloom(inp){
 	
-query({"inputs": inp, "parameters":{"do_sample": do_sample}}).then((response) => { 
+query({"inputs": inp}).then((response) => { 
 
 //tk.flash(response[0].generated_text)
 alert(inp)
